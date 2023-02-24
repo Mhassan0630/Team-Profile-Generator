dd40304d018796bb7442771f134db5685cabb277
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-02-23 23:17:09 -0600

    fixing index.js

--- index.js
+++ index.js
@@ -1,44 +1,119 @@
-const intern = new Intern(
-  answers.name,
-  answers.id,
-  answers.email,
-  answers.school
-);
-teamMembers.push(intern);
-addTeamMember();
-});
+const fs = require("fs");
+const inquirer = require("inquirer");
+const jest = require("jest");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const render = require("./src/page-template");
+
+const teamMembers = [];
+
+const addManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your manager's name?",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter a name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your manager's employee ID?",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log("Please enter an employee ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your manager's email address?",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter an email address!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your manager's office number?",
+        validate: (officeNumberInput) => {
+          if (officeNumberInput) {
+            return true;
+          } else {
+            console.log("Please enter an office number!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamMembers.push(manager);
+      addTeamMember();
+    });
 };
 
-const addTeamMember = () => {
-return inquirer
-.prompt([
-{
-  type: "list",
-  name: "memberChoice",
-  message: "What type of team member would you like to add?",
-  choices: ["Engineer", "Intern", "I don't want to add any more team members"],
-},
-])
-.then((userChoice) => {
-switch (userChoice.memberChoice) {
-  case "Engineer":
-    addEngineer();
-    break;
-  case "Intern":
-    addIntern();
-    break;
-  default:
-    writeFile(teamMembers);
-    break;
-}
-});
-};
-
-const writeFile = (data) => {
-fs.writeFile("./dist/index.html", render(data), (err) => {
-if (err) throw err;
-console.log("Your team profile has been successfully generated!");
-});
-};
-
-addManager();
+const addEngineer = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your engineer's name?",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter a name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your engineer's employee ID?",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log("Please enter an employee ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your engineer's email address?",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter an email address!");
+            return false;
+          }
+       
